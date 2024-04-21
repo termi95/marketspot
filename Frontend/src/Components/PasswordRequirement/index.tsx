f62a541a0825794447a3795c8b7876cbd76b0d43
DESKTOP-6Q4VF0O\Termi <sokolowski.a95@gmail.com>
2024-04-21 21:45:00 +0200

    fix password validator

--- Frontend/src/Components/PasswordRequirement/index.tsx
+++ Frontend/src/Components/PasswordRequirement/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Text, rem } from "@mantine/core";
+import { Box, rem } from "@mantine/core";
 import { IconCheck, IconX } from "@tabler/icons-react";
 
 function PasswordRequirement({
@@ -9,7 +9,7 @@ function PasswordRequirement({
   label: string;
 }) {
   return (
-    <Text
+    <Box
       c={meets ? "teal" : "red"}
       style={{ display: "flex", alignItems: "center" }}
       mt={7}
@@ -21,7 +21,7 @@ function PasswordRequirement({
         <IconX style={{ width: rem(14), height: rem(14) }} />
       )}{" "}
       <Box ml={10}>{label}</Box>
-    </Text>
+    </Box>
   );
 }
 
