66cf856d2676814c0a70da24733ba99df769f14c
DESKTOP-6Q4VF0O\Termi <sokolowski.a95@gmail.com>
2024-11-22 21:14:06 +0100

    reajust navbar

--- Frontend/src/Components/TopBar/index.tsx
+++ Frontend/src/Components/TopBar/index.tsx
@@ -1,4 +1,4 @@
-import { Group } from "@mantine/core";
+import { Flex, Grid } from "@mantine/core";
 import Logo from "../Logo";
 import SearchBar from "../SearchBar";
 import SignInOrLoggedIn from "../SignInOrLoggedIn";
@@ -9,11 +9,19 @@ function TopBar() {
   const { isMobile } = useSelector((state: RootState) => state.user);
   return (
     <nav style={{backgroundColor:"var(--main-color)"}}>
-      <Group gap="xs" grow preventGrowOverflow={!isMobile}>
+    <Grid align="center" justify="center" overflow="hidden">
+      <Grid.Col span={{base: 6, sm: 6, md: 4}} order={{base: 1, xs:1, sm: 1, md: 1 }}>
         <Logo />
+      </Grid.Col>
+      <Grid.Col span={{ sm: 12, md: 4}} offset={isMobile ? -1 : 0}  order={{base: 3, xs:3,  sm: 3, md: 2 }}>
         <SearchBar />
+      </Grid.Col>
+      <Grid.Col span={{base: 4, sm: 6, md: 4}}  order={{base: 2, xs:2, sm: 2, md: 3 }}>
+        <Flex justify={ isMobile ? "center" : "flex-end"}>
         <SignInOrLoggedIn />
-      </Group>
+        </Flex>
+      </Grid.Col>
+    </Grid>
     </nav>
   );
 }
