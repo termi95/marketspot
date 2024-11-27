4e29f6f538c1de87371550742fad5aa60d81a749
DESKTOP-6Q4VF0O\Termi <sokolowski.a95@gmail.com>
2024-11-27 19:36:01 +0100

    change to layout

--- Frontend/src/Components/MainPanel/index.tsx
+++ Frontend/src/Components/MainPanel/index.tsx
@@ -2,19 +2,16 @@ import { Container, Flex } from "@mantine/core";
 import TopBar from "../TopBar";
 import BottomBar from "../BottomBar";
 import React from "react";
-import { useSelector } from "react-redux";
-import { RootState } from "../../State/store";
 
 function MainPanel({ children }: { children: React.ReactNode }) {
-  const { isMobile } = useSelector((state: RootState) => state.user);
   return (
     <Flex justify="space-between" direction="column" style={{ gap: "unset" }} mih={"100dvh"}>
       <TopBar />
       <Container
-        w={isMobile ? "99dvw":"95dvw"}
+        w={"99dvw"}
         p={0}
         bg={"var(--background-color)"}
-        maw={isMobile ? "99dvw":"95dvw"}
+        maw={"99dvw"}
         flex={1}
       >
         {children}
